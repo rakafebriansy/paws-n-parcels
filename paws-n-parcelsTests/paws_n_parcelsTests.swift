368847cff220f88f44b6f0824c1c736c2ff1f6e1
rakafebriansy <rakafebrian19@gmail.com>
2026-05-04 22:15:55 +0700

    init project

---
+++ paws-n-parcelsTests/paws_n_parcelsTests.swift
@@ -0,0 +1,19 @@
+//
+//  paws_n_parcelsTests.swift
+//  paws-n-parcelsTests
+//
+//  Created by Raka Febrian Syahputra on 04/05/26.
+//
+
+import Testing
+@testable import paws_n_parcels
+
+struct paws_n_parcelsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
