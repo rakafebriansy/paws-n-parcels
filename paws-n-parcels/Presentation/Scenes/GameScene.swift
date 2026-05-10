b86d09751f0559abf47bae0a4329bbf488965162
rakafebriansy <rakafebrian19@gmail.com>
2026-05-10 17:31:05 +0700

    feat: setup grid system

--- paws-n-parcels/Presentation/Scenes/GameScene.swift
+++ paws-n-parcels/Presentation/Scenes/GameScene.swift
@@ -12,79 +12,65 @@ import SwiftUI
 
 class GameScene: SKScene {
     
-    // Gameplay Kit System
     var playerEntity: PlayerEntity!
     var movementSystem = GKComponentSystem<MovementComponent>(componentClass: MovementComponent.self)
-    var previousTime: TimeInterval = 0
     
-    // Camera & UI
     let cameraNode = SKCameraNode()
-    var joystickBase: SKShapeNode!
-    var joystickKnob: SKShapeNode!
-    var isJoystickActive = false
+    let joystick = JoystickController()
+    var mapBuilder: MapBuilder!
     
-    override init() {
-        super.init()
-    }
-    
-    override init(size: CGSize) {
-        super.init(size: size)
-    }
-    
-    required init?(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
+    var previousTime: TimeInterval = 0
     
     override func didMove(to view: SKView) {
         self.anchorPoint = CGPoint(x: 0.5, y: 0.5)
+        self.camera = cameraNode
+        addChild(cameraNode)
         
-        setupMap()
+        mapBuilder = MapBuilder(scene: self, gridSize: 100)
+        mapBuilder.build(blueprint: worldMap)
+        
         setupPlayer()
-        setupCameraAndJoystick()
+        joystick.attach(to: cameraNode, screenHeight: self.size.height)
+        
+        drawDebugGrid(gridSize: 100)
     }
     
-    // MARK: - Setup ground and road
-    func setupMap() {
+    // MARK: - Grid support line
+    func drawDebugGrid(gridSize: CGFloat) {
+        let path = CGMutablePath()
         
-        // draw ground and road on canvas
-        let ground = SKShapeNode(rectOf: CGSize(width: 2000, height: 2000), cornerRadius: 50)
-        ground.fillColor = .systemGreen
-        ground.strokeColor = .clear
-        ground.zPosition = -10
-        ground.position = CGPoint(x: 0, y: 0)
-        addChild(ground)
+        let worldRadius: CGFloat = 2500
+        let start = -worldRadius
+        let end = worldRadius
         
-        let homeA = CGPoint(x: -80, y: 70)
-        let homeB = CGPoint(x: 100, y: -10)
+        for x in stride(from: start, through: end, by: gridSize) {
+            path.move(to: CGPoint(x: x, y: start))
+            path.addLine(to: CGPoint(x: x, y: end))
+        }
         
-        let roadPath = CGMutablePath()
-        roadPath.move(to: homeA)
-        roadPath.addLine(to: homeB)
+        for y in stride(from: start, through: end, by: gridSize) {
+            path.move(to: CGPoint(x: start, y: y))
+            path.addLine(to: CGPoint(x: end, y: y))
+        }
         
-        let road = SKShapeNode(path: roadPath)
-        road.strokeColor = .systemGray
-        road.lineWidth = 40
-        road.lineCap = .round
-        road.lineJoin = .round
-        road.zPosition = -5
-        addChild(road)
+        let gridNode = SKShapeNode(path: path)
         
-        // insert home object
-        drawHome(at: homeA, color: .systemOrange)
-        drawHome(at: homeB, color: .systemBlue)
-    }
-    
-    // MARK: - Draw home object
-    func drawHome(at point: CGPoint, color: UIColor) {
+        gridNode.strokeColor = UIColor.white.withAlphaComponent(0.3)
+        gridNode.lineWidth = 2
+        gridNode.zPosition = 90
+        addChild(gridNode)
         
-        // draw home on canvas
-        let home = SKShapeNode(rectOf: CGSize(width: 50, height: 50), cornerRadius: 10)
-        home.position = point
-        home.fillColor = color
-        home.strokeColor = .white
-        home.lineWidth = 3
-        home.zPosition = 1
-        addChild(home)
+        let centerPath = CGMutablePath()
+        centerPath.move(to: CGPoint(x: -50, y: 0))
+        centerPath.addLine(to: CGPoint(x: 50, y: 0))
+        centerPath.move(to: CGPoint(x: 0, y: -50))
+        centerPath.addLine(to: CGPoint(x: 0, y: 50))
+        
+        let centerNode = SKShapeNode(path: centerPath)
+        centerNode.strokeColor = .red
+        centerNode.lineWidth = 5
+        centerNode.zPosition = 91
+        addChild(centerNode)
     }
     
     // MARK: - Setup character & ECS
@@ -97,6 +83,11 @@ class GameScene: SKScene {
         playerNode.lineWidth = 3
         playerNode.zPosition = 5
         playerNode.position = CGPoint(x: 0, y: 0)
+        
+        // add physics body
+        playerNode.physicsBody = SKPhysicsBody(circleOfRadius: 25)
+        playerNode.physicsBody?.affectedByGravity = false
+        playerNode.physicsBody?.allowsRotation = false
         addChild(playerNode)
         
         // insert visual into entity
@@ -104,78 +95,34 @@ class GameScene: SKScene {
         movementSystem.addComponent(foundIn: playerEntity)
     }
     
-    // MARK: - Setup camera & joystick
-    func setupCameraAndJoystick() {
-        
-        // camera
-        self.camera = cameraNode
-        addChild(cameraNode)
-        
-        // joystick base
-        joystickBase = SKShapeNode(circleOfRadius: 50)
-        joystickBase.fillColor = UIColor.black.withAlphaComponent(0.2)
-        joystickBase.strokeColor = .clear
-        joystickBase.position = CGPoint(x: 0, y: -250) // bottom of screen
-        joystickBase.zPosition = 100 // bring to front
-        cameraNode.addChild(joystickBase)
-        
-        // joystick knob
-        joystickKnob = SKShapeNode(circleOfRadius: 25)
-        joystickKnob.fillColor = .white
-        joystickKnob.strokeColor = .clear
-        joystickKnob.zPosition = 101
-        joystickBase.addChild(joystickKnob)
-    }
-    
-    // MARK: - Touch Input
+    // MARK: - Handling Joystick
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         guard let touch = touches.first else { return }
         let location = touch.location(in: cameraNode)
+        let treshold = -(self.size.height / 4)
         
-        if joystickBase.contains(location) {
-            isJoystickActive = true
-        }
+        joystick.processTouchBegan(location: location, treshold: treshold)
     }
     
-    // MARK: - Joystick Logic
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        guard isJoystickActive, let touch = touches.first else { return }
-        let location = touch.location(in: joystickBase)
+        guard let touch = touches.first else { return }
+        let locationInBase = touch.location(in: joystick.baseNode)
         
-        // set knob bounds
-        let maxRadius: CGFloat = 50.0
-        let distance = sqrt(location.x * location.x + location.y * location.y)
+        joystick.processTouchMoved(locationInBase: locationInBase)
         
-        var newPosition = location
-        if distance > maxRadius {
-            let ratio: CGFloat = maxRadius / distance
-            newPosition.x *= ratio
-            newPosition.y *= ratio
-        }
-        joystickKnob.position = newPosition
-        
-        let velocityX = newPosition.x / maxRadius
-        let velocityY = newPosition.y / maxRadius
-    
         if let movement = playerEntity.component(ofType: MovementComponent.self) {
-            movement.velocity = CGPoint(x: velocityX, y: velocityY)
+            movement.velocity = joystick.currentVelocity
         }
     }
     
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        joystick.processTouchEnded()
         
-        // reset joystick on release
-        isJoystickActive = false
-        let resetAction = SKAction.move(to: .zero, duration: 0.1)
-        resetAction.timingMode = .easeOut
-        joystickKnob.run(resetAction)
-        
-        // stop gameplay kit movement
         if let movement = playerEntity.component(ofType: MovementComponent.self) {
-            movement.velocity = .zero
+            movement.velocity = joystick.currentVelocity
         }
     }
-    
+ 
     // MARK: - Update Loop
     override func update(_ currentTime: TimeInterval) {
         if previousTime == 0 {
@@ -186,8 +133,8 @@ class GameScene: SKScene {
         
         movementSystem.update(deltaTime: deltaTime)
         
-        if let renderNode = playerEntity.component(ofType: RenderComponent.self)?.node {
-            cameraNode.position = renderNode.position
+        if let playerNode = playerEntity.component(ofType: RenderComponent.self)?.node {
+            cameraNode.position = playerNode.position
         }
     }
 }
