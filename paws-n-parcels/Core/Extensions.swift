5efd9514e5205a99284e913c0da7f8e284077b1a
Jennifer <aloysiajennifer@gmail.com>
2026-05-15 11:52:34 +0700

    feat: add UI main menu

--- paws-n-parcels/Core/Extensions.swift
+++ paws-n-parcels/Core/Extensions.swift
@@ -13,6 +13,9 @@ extension Color {
     static let cream = Color(red: 0.98, green: 0.96, blue: 0.9)
     static let brown = Color(red: 0.4, green: 0.25, blue: 0.15)
     static let sage = Color(red: 197/255, green: 218/255, blue: 179/255)
+    static let darkGray = Color(red: 65/255, green: 55/255, blue: 48/255)
+    static let red = Color(red: 223/255, green: 74/255, blue: 80/255)
+
 }
 
 // Font
---
+++ paws-n-parcels/Presentation/UI/MainMenuModalView.swift
@@ -0,0 +1,110 @@
+
+//
+//  MainMenuModalView.swift
+//  paws-n-parcels
+//
+//  Created by Aloysia Jennifer on 12/05/26.
+//
+
+import SwiftUI
+
+struct MainMenuModalView: View {
+    @AppStorage("bgm") private var bgm = 100.0
+    @AppStorage("sfx") private var sfx = 100.0
+    @AppStorage("isVibrationOn") private var isVibrationOn = true
+    @State private var isEditing = false
+    
+    var body: some View {
+        ZStack {
+            Image("modal").resizable().scaledToFit().padding(24)
+            
+            VStack(spacing: 23) {
+                Text("Menu")
+                    .comicRelief(size: 45, isBold: true)
+                    .foregroundColor(.darkGray)
+                    .multilineTextAlignment(.center)
+                
+                VStack(spacing: 15) {
+                    Button(action: {
+                        
+                    }){
+                        HStack {
+                            Image(systemName: "book.fill")
+                            Text("Collectibles")
+                        }
+                        .comicRelief(size: 25, isBold: true)
+                        .tracking(1.5)
+                        .foregroundColor(Color.cream)
+                        .padding(.vertical, 8)
+                        .padding(.horizontal, 30)
+                        .background(Color.darkGray)
+                        .cornerRadius(35)
+                    }
+                    
+                    Button(action: {
+                        
+                    }){
+                        HStack {
+                            Image(systemName: "person.2.fill")
+                            Text("Relationships")
+                        }
+                        .comicRelief(size: 25, isBold: true)
+                        .tracking(1.5)
+                        .foregroundColor(Color.cream)
+                        .padding(.vertical, 8)
+                        .padding(.horizontal, 20)
+                        .background(Color.darkGray)
+                        .cornerRadius(35)
+                    }
+                }
+                
+                HStack {
+                    Image(systemName: bgm == 0 ? "music.note.slash" : "music.note")
+                        .foregroundColor(.red)
+                        .font(.system(size:28))
+                    
+                    Text("BGM")
+                        .comicRelief(size: 25, isBold: true)
+                        .foregroundColor(.darkGray)
+                    
+                    Slider(
+                        value: $bgm,
+                        in: 0...100,
+                        onEditingChanged: { editing in
+                            isEditing = editing
+                        }
+                    ).frame(width:130)
+                        .tint(Color.darkGray)
+                }
+                
+                HStack {
+                    Image(systemName: sfx == 0 ? "speaker.slash.fill" : "speaker.wave.2.fill")
+                        .foregroundColor(.red)
+                        .font(.system(size:28))
+                    
+                    Text("SFX")
+                        .comicRelief(size: 25, isBold: true)
+                        .foregroundColor(.darkGray)
+                    
+                    Slider(
+                        value: $sfx,
+                        in: 0...100,
+                        onEditingChanged: { editing in
+                            isEditing = editing
+                        }
+                    ).frame(width:130)
+                        .tint(Color.darkGray)
+                }
+            }
+            .offset(y: -25)
+        }
+        .transition(.scale.combined(with: .opacity))
+    }
+}
+
+#Preview {
+    ZStack {
+        Color.black.opacity(0.3).ignoresSafeArea()
+        MainMenuModalView()
+    }
+}
