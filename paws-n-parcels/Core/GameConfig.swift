21112c50b594d4f98c683c6391d6c1178ab741bd
Jennifer <aloysiajennifer@gmail.com>
2026-05-19 21:21:54 +0700

    feat: rapihin tampilan modal, ganti gambar paket pakai asset asli

--- paws-n-parcels/Core/GameConfig.swift
+++ paws-n-parcels/Core/GameConfig.swift
@@ -9,7 +9,7 @@ import Foundation
 
 struct GameConfig {
     static let deliveryRewardPoints: Int = 1
-    static let pointsForAcquaintance: Int = 5
+    static let pointsForAcquaintance: Int = 1
     static let pointsForFriend: Int = 10
     static let pointsForCloseFriend: Int = 20
     static let pointsForBestFriend: Int = 40
---
+++ paws-n-parcels/Services/AIService 2.swift
@@ -0,0 +1,8 @@
+//
+//  AIService.swift
+//  paws-n-parcels
+//
+//  Created by Raka Febrian Syahputra on 06/05/26.
+//
+
+import Foundation
